e6abbbf596604e1a1b68300def68bb91c2c867ae
Martin Villalba <14tinchov@gmail.com>
2019-06-15 07:03:51 -0300

    Initial commit

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,16 +1,39 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const request = require('request');
 
+function textResponse(msg){
+  return {
+    "fulfillmentText": "Full Text response",
+    "fulfillmentMessages": [
+      {
+        "text": {
+          "text": [msg]
+        }
+      }
+    ],
+    "source": "<Text response>"
+  }
+};
+
+module.exports = function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
+        request.get({
+            url: 'https://jsonplaceholder.typicode.com/todos/1',
+        }, function (error, response, body) {
+            const respObj = JSON.parse(body);
+            const respMsg = textResponse(respObj.title);
+
+            context.res = {
+                    status: 200,
+                    body: respMsg
+                };
+
+            context.done(null, respMsg);
+        });
+      } else {
+       context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
+           body: "Please pass a name on the query string or in the request body"
+      };
+      context.done(null, '');
     }
 };
